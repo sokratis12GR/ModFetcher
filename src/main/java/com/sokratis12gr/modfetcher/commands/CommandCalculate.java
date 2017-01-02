5757107fc7b218fb19479b09105ac259cdf95852
Sokratis Fotkatzikis <sokratis12GR@gmail.com>
2017-01-02 20:41:31 +0200

    Tweaked Calculations, Improved them, added 4 number support

--- src/main/java/com/sokratis12gr/modfetcher/commands/CommandCalculate.java
+++ src/main/java/com/sokratis12gr/modfetcher/commands/CommandCalculate.java
@@ -1,16 +1,19 @@
 package com.sokratis12gr.modfetcher.commands;
 
+import com.sokratis12gr.modfetcher.util.Utilities;
 import sx.blah.discord.handle.obj.IMessage;
 
 import java.util.regex.Pattern;
 
 import static com.sokratis12gr.modfetcher.util.Utilities.sendMessage;
+import static java.lang.Math.*;
 
 public class CommandCalculate implements Command {
 
-    private char calcTypeOne;
-    private char calcTypeTwo;
-    private double[] numbers = new double[]{0, 0, 0};
+    private String calcTypeOne;
+    private String calcTypeTwo;
+    private String calcTypeThree;
+    private double[] numbers = new double[]{0, 0, 0, 0};
     private Number calculated;
 
     @Override
@@ -23,17 +26,25 @@ public class CommandCalculate implements Command {
             if (!Pattern.matches(".*[a-zA-Z].*", args[1])) {
                 numbersIn[0] = Double.valueOf(args[1]);
                 if (args.length > 2) {
-                    calcTypeOne = args[2].charAt(0);
+                    calcTypeOne = args[2];
                     if (args.length > 3) {
                         numbersIn[1] = Double.valueOf(args[3]);
-                        doCalculations(false);
+                        doCalculations(false, false);
                         description = "Result: " + getCalculations();
                         if (args.length > 4) {
-                            calcTypeTwo = args[4].charAt(0);
+                            calcTypeTwo = args[4];
                             if (args.length > 5) {
                                 numbersIn[2] = Double.valueOf(args[5]);
-                                doCalculations(true);
+                                doCalculations(true, false);
                                 description = "Result: " + getCalculations();
+                                if (args.length > 6) {
+                                    calcTypeThree = args[6];
+                                    if (args.length > 7) {
+                                        numbersIn[3] = Double.valueOf(args[7]);
+                                        doCalculations(true, true);
+                                        description = "Result: " + getCalculations();
+                                    }
+                                }
                             }
                         }
                     }
@@ -45,38 +56,98 @@ public class CommandCalculate implements Command {
         sendMessage(message.getChannel(), description);
     }
 
-    private double getCalculations() {
-        return (double) calculated;
+    private Number getCalculations() {
+        return calculated;
     }
 
-    private void doCalculations(boolean secondCalc) {
+    private void doCalculations(boolean secondCalc, boolean thirdCalc) {
         Number result = 0;
         Number numberOne = 0;
         double first = numbers[0];
         double second = numbers[1];
         switch (calcTypeOne) {
-            case '+':
+            case "+":
                 numberOne = first + second;
                 result = numberOne;
                 break;
-            case '-':
+            case "add":
+                numberOne = first + second;
+                result = numberOne;
+                break;
+            case "plus":
+                numberOne = first + second;
+                result = numberOne;
+                break;
+            case "-":
                 numberOne = first - second;
                 result = numberOne;
                 break;
-            case '*':
+            case "minus":
+                numberOne = first - second;
+                result = numberOne;
+                break;
+            case "sub":
+                numberOne = first - second;
+                result = numberOne;
+                break;
+            case "*":
                 numberOne = first * second;
                 result = numberOne;
                 break;
-            case '/':
+            case "multiply":
+                numberOne = first * second;
+                result = numberOne;
+                break;
+            case "times":
+                numberOne = first * second;
+                result = numberOne;
+                break;
+            case "/":
                 numberOne = first / second;
                 result = numberOne;
                 break;
-            case '^':
-                numberOne = Math.pow(first, second);
+            case "divide":
+                numberOne = first / second;
                 result = numberOne;
                 break;
-            case '%':
+            case "^":
+                numberOne = pow(first, second);
+                result = numberOne;
+                break;
+            case "%":
                 numberOne = first % second;
+                result = numberOne;
+                break;
+            case "modulus":
+                numberOne = first % second;
+                result = numberOne;
+                break;
+            case "√":
+                numberOne = pow(first, 1 / second);
+                result = numberOne;
+                break;
+            case "root":
+                numberOne = pow(first, 1 / second);
+                result = numberOne;
+                break;
+            case "+e":
+                numberOne = addExact((long) first, (long) second);
+                result = numberOne;
+                break;
+            case "-e":
+                numberOne = subtractExact((long) first, (long) second);
+                result = numberOne;
+                break;
+            case "*e":
+                numberOne = multiplyExact((long) first, (long) second);
+                result = numberOne;
+                break;
+            case "max":
+                numberOne = max(first, second);
+                result = numberOne;
+                break;
+            case "min":
+                numberOne = min(first, second);
                 result = numberOne;
                 break;
         }
@@ -84,38 +155,201 @@ public class CommandCalculate implements Command {
             Number numberTwo = 0;
             double third = numbers[2];
             switch (calcTypeTwo) {
-                case '+':
+                case "+":
                     numberTwo = (double) numberOne + third;
                     result = numberTwo;
                     break;
-                case '-':
+                case "add":
+                    numberTwo = (double) numberOne + third;
+                    result = numberTwo;
+                    break;
+                case "plus":
+                    numberTwo = (double) numberOne + third;
+                    result = numberTwo;
+                    break;
+                case "-":
                     numberTwo = (double) numberOne - third;
                     result = numberTwo;
                     break;
-                case '*':
+                case "minus":
+                    numberTwo = (double) numberOne - third;
+                    result = numberTwo;
+                    break;
+                case "sub":
+                    numberTwo = (double) numberOne - third;
+                    result = numberTwo;
+                    break;
+                case "*":
                     numberTwo = (double) numberOne * third;
                     result = numberTwo;
                     break;
-                case '/':
+                case "multiply":
+                    numberTwo = (double) numberOne * third;
+                    result = numberTwo;
+                    break;
+                case "times":
+                    numberTwo = (double) numberOne * third;
+                    result = numberTwo;
+                    break;
+                case "/":
                     numberTwo = (double) numberOne / third;
                     result = numberTwo;
                     break;
-                case '^':
-                    numberTwo = Math.pow((double) numberOne, third);
+                case "divide":
+                    numberTwo = (double) numberOne / third;
                     result = numberTwo;
                     break;
-                case '%':
+                case "^":
+                    numberTwo = pow((double) numberOne, third);
+                    result = numberTwo;
+                    break;
+                case "%":
                     numberTwo = (double) numberOne % third;
                     result = numberTwo;
                     break;
+                case "modulus":
+                    numberTwo = (double) numberOne % third;
+                    result = numberTwo;
+                    break;
+                case "√":
+                    numberTwo = pow((double) numberOne, 1 / third);
+                    result = numberTwo;
+                    break;
+                case "root":
+                    numberTwo = pow((double) numberOne, 1 / third);
+                    result = numberTwo;
+                    break;
+                case "+e":
+                    numberTwo = addExact((long) numberOne, (long) third);
+                    result = numberTwo;
+                    break;
+                case "-e":
+                    numberTwo = subtractExact((long) numberOne, (long) third);
+                    result = numberTwo;
+                    break;
+                case "*e":
+                    numberTwo = multiplyExact((long) numberOne, (long) third);
+                    result = numberTwo;
+                    break;
+                case "max":
+                    numberTwo = max((double) numberOne, third);
+                    result = numberTwo;
+                    break;
+                case "min":
+                    numberTwo = min((double) numberOne, third);
+                    result = numberTwo;
+                    break;
+            }
+            if (thirdCalc) {
+                Number numberThree = 0;
+                double four = numbers[3];
+                switch (calcTypeThree) {
+                    case "+":
+                        numberThree = (double) numberTwo + four;
+                        result = numberThree;
+                        break;
+                    case "add":
+                        numberThree = (double) numberTwo + four;
+                        result = numberThree;
+                        break;
+                    case "plus":
+                        numberThree = (double) numberTwo + four;
+                        result = numberThree;
+                        break;
+                    case "-":
+                        numberThree = (double) numberTwo - four;
+                        result = numberThree;
+                        break;
+                    case "minus":
+                        numberThree = (double) numberTwo - four;
+                        result = numberThree;
+                        break;
+                    case "sub":
+                        numberThree = (double) numberTwo - four;
+                        result = numberThree;
+                        break;
+                    case "*":
+                        numberThree = (double) numberTwo * four;
+                        result = numberThree;
+                        break;
+                    case "multiply":
+                        numberThree = (double) numberTwo * four;
+                        result = numberThree;
+                        break;
+                    case "times":
+                        numberThree = (double) numberTwo * four;
+                        result = numberThree;
+                        break;
+                    case "/":
+                        numberThree = (double) numberTwo / four;
+                        result = numberThree;
+                        break;
+                    case "divide":
+                        numberThree = (double) numberTwo / four;
+                        result = numberThree;
+                        break;
+                    case "^":
+                        numberThree = pow((double) numberTwo, four);
+                        result = numberThree;
+                        break;
+                    case "%":
+                        numberThree = (double) numberTwo % four;
+                        result = numberThree;
+                        break;
+                    case "modulus":
+                        numberThree = (double) numberTwo % four;
+                        result = numberThree;
+                        break;
+                    case "√":
+                        numberThree = pow((double) numberTwo, 1 / four);
+                        result = numberThree;
+                        break;
+                    case "root":
+                        numberThree = pow((double) numberTwo, 1 / four);
+                        result = numberThree;
+                        break;
+                    case "+e":
+                        numberThree = addExact((long) numberTwo, (long) four);
+                        result = numberThree;
+                        break;
+                    case "-e":
+                        numberThree = subtractExact((long) numberTwo, (long) four);
+                        result = numberThree;
+                        break;
+                    case "*e":
+                        numberThree = multiplyExact((long) numberTwo, (long) four);
+                        result = numberThree;
+                        break;
+                    case "max":
+                        numberThree = max((double) numberTwo, four);
+                        result = numberThree;
+                        break;
+                    case "min":
+                        numberThree = min((double) numberTwo, four);
+                        result = numberThree;
+                        break;
+                }
             }
         }
-        calculated = (double) result <= Double.MIN_VALUE || (double) result >= Double.MAX_VALUE ? 0 : (double) result;
+        calculated = result;
     }
 
     @Override
     public String getDescription() {
-        return "Does some basic calculations.Types: \"\'+\' , \'-\', \'*\', \'/\' \"";
+        return "Does calculations.\nTypes:"
+                + Utilities.makeMultiCodeBlock("\"+\" or \"add\" or \"plus\""
+                + "\n\"-\" or \"sub\" or \"minus\""
+                + "\n\"*\" or \"times\" or \"multiply\""
+                + "\n\"/\" or \"divide\""
+                + "\n\"^\""
+                + "\n\"%\" or \"modulus\""
+                + "\n\"√\" or \"root\""
+                + "\n\"+e\""
+                + "\n\"-e\""
+                + "\n\"*e\""
+                + "\n\"max\""
+                + "\n\"min\""
+        );
     }
 
     @Override
